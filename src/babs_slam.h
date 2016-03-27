56ebe136f9bdea86f2dbc20ad95a074b19b13a37
nrgsy <tpech46@gmail.com>
2016-03-27 18:21:45 -0400

    new functions in babs_slam

--- src/babs_slam.h
+++ src/babs_slam.h
@@ -2,6 +2,11 @@
 #define BABS_SLAM_H_
 
 #include <ros/ros.h>
+#include <geometry_msgs/Pose.h>
+#include <nav_msgs/Odometry.h>
+#include <nav_msgs/OccupancyGrid.h>
+#include <math.h>
+
 class babs_slam
 {
 public:
@@ -9,7 +14,19 @@ public:
 
 
 private:
+
+	geometry_msgs::Pose sampleMotionModel(nav_msgs::Odometry state, double params[]);
+	double sample_normal(double bSquared);
+	double convertPlanarQuat2Phi(geometry_msgs::Quaternion quaternion);
+	geometry_msgs::Quaternion convertPlanarPhi2Quaternion(double phi);
+
+
+
+
 	ros::NodeHandle nh_;
+
+	//TODO use a timer or something instead of hard coding
+	double dt = 0.1;// time since last running of SLAM,
 
 };
 
