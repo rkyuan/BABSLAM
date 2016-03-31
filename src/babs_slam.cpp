909c996fb7b22545560ff270b8f0a7b7c6ab3db4
sergiy-turchyn <sergey.turchyn@gmail.com>
2016-03-31 19:35:20 -0400

    Added measurement model probability functions

--- src/babs_slam.cpp
+++ src/babs_slam.cpp
@@ -121,6 +121,45 @@ void babs_slam::resample(std::vector<float> weights){
 	particles = newParticles;
 }
 
+// Sensor model probabilities
+
+// eq 6.4-6.6 form the book
+float babs_slam::pHit(float z, float trueZ) {
+	if (z<0 || z>MAX_LIDAR_RANGE)
+		return 0;
+	float gaussianProb = (1/sqrt(2*M_PI*pow(STD_HIT,2)))*exp(-0.5*pow((z-trueZ),2)/pow(STD_HIT,2));
+	float normalizer = 1; // Supposed to be an integral, but it only matters when measured value is close to min or max
+	return gaussianProb*normalizer;
+}
+
+// eq 6.7-6.9 from the book
+float babs_slam::pShort(float z, float trueZ) {
+	if (z<0 || z>trueZ || z>MAX_LIDAR_RANGE)
+		return 0;
+	float expProb = L_SHORT*exp(-L_SHORT*z);
+	float normalizer = 1/(1-exp(-L_SHORT*trueZ));
+	return expProb*normalizer;
+}
+
+// eq 6.10 from the book
+float babs_slam::pMax(float z) {
+	if (babs_slam::compareFloats(z, MAX_LIDAR_RANGE))
+		return 1;
+	return 0;
+}
+
+// eq 6.11 from the book
+float babs_slam::pRand(float z) {
+	if (z<0 || z>MAX_LIDAR_RANGE)
+		return 0;
+	return 1/MAX_LIDAR_RANGE;
+}
+
+bool babs_slam::compareFloats(float a, float b) {
+	if (fabs(a-b) < 0.001)
+		return true;
+	return false;
+}
 
 void babs_slam::encoder_callback(const std_msgs::Float64& message_holder){
 
@@ -146,7 +185,16 @@ int main(int argc, char** argv)
 
     
     babs_slam babs(&nh); 
-
+	/*
+	// Sensor model testing code
+	for (float i=0; i<11; i+=0.05) {
+		float phit = babs.pHit(i,5);
+		float pshort = babs.pShort(i, 5);
+		float pmax = babs.pMax(i);
+		float prand = babs.pRand(i);
+		ROS_INFO("i=%f, phit=%f, pshort=%f, pmax=%f, prand=%f",i,phit, pshort,pmax,prand);
+	}
+	*/
     
     ros::spin();
     return 0;
--- src/babs_slam.h
+++ src/babs_slam.h
@@ -14,7 +14,10 @@ class babs_slam
 {
 public:
 	babs_slam(ros::NodeHandle* nodehandle);
-
+	float pHit(float z, float trueZ);
+	float pShort(float z, float trueZ);
+	float pMax(float z);
+	float pRand(float z);
 
 private:
 
@@ -27,7 +30,14 @@ private:
 	ros::NodeHandle nh_;
 	std::vector<geometry_msgs::Pose> particles;
 	nav_msgs::OccupancyGrid map;
-
+	// Measurement model parameters
+	const float Z_HIT = 1;
+	const float Z_SHORT = 0;
+	const float Z_MAX = 0;
+	const float Z_RAND = 0;
+	const float STD_HIT = 0.05;
+	const float L_SHORT = 0.1;
+	const float MAX_LIDAR_RANGE = 10;
 
 	void update();
 	geometry_msgs::Pose sampleMotionModel(geometry_msgs::Pose p);
@@ -40,6 +50,8 @@ private:
 	static void gps_callback(const std_msgs::Float64& message_holder);
 	static void lidar_callback(const std_msgs::Float64& message_holder);
 
+	bool compareFloats(float a, float b);
+	
 	//TODO use a timer or something instead of hard coding
 	double dt = 0.1;// time since last running of SLAM,
 
